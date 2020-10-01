d3a399a72ed32648ac309ff2cd61c6a38ab451a8
gburich <gabriel.burich@suntech.com.br>
2020-10-01 14:08:47 -0300

    [Order] - Salvando pedido no banco

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,15 +1,19 @@
-import {responseError, responseHttpOk} from "../libs/responseUtils";
+import {responseCreate, responseError} from "../libs/responseUtils";
 
 module.exports = server => {
 
-    //TODO save order
     server.route('/order')
         .all(server.auth.authenticate())
         .post((req, res) => {
-            const {restaurantId, filter} = req.body;
+            const {restaurantId, meal, filter} = req.body;
             const {acceptsMealVoucher, typeRestaurantId, orderTime, distance, typeOrderId, sitPlace} = filter;
 
-            server.db.none(
+            const promiseOrder = server.db.one(
+                'INSERT INTO "order" (user_id, restaurant_id, meal_id) values ($1, $2, $3) RETURNING id;',
+                [req.user.id, restaurantId, meal.id]
+            )
+
+            const lastCasePromise = server.db.none(
                 'INSERT INTO ' +
                 'last_cases ' +
                 '(accepts_meal_voucher, tp_restaurant_id, order_time, distance, tp_order_id, sit_place, restaurant_id) ' +
@@ -17,7 +21,9 @@ module.exports = server => {
                 '($1, $2, $3, $4, $5, $6, $7);',
                 [acceptsMealVoucher, typeRestaurantId, orderTime, distance, typeOrderId, sitPlace, restaurantId]
             )
-                .then(data => responseHttpOk(data, res))
+
+            Promise.all([promiseOrder, lastCasePromise])
+                .then(data => responseCreate(data[0], res))
                 .catch(error => responseError(error, res));
         })
 
